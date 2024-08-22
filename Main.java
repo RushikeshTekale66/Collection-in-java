59ad6143ec24697f722365adff129167e17867e9
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-08-22 20:14:00 +0530

    List : ArrayList

--- Main.java
+++ Main.java
@@ -3,50 +3,28 @@ import java.util.ArrayList;
 public class Main
 {
 	public static void main(String[] args) {
-		List list1 = new ArrayList();
+	    
+	    //Array List
+		List listArray = new ArrayList();
 		
-		//Add element to list
-		list1.add("Rushikesh");
-		list1.add(7887);
-		System.out.println(list1);
+		//1 .add();
+		listArray.add("Rushikesh Tekale");
+		listArray.add(24);
+		listArray.add("Pune");
 		
-		//Add to given index 
-		list1.add(1, "Tekale");
-		System.out.println(list1);
+		//2 .indexOf();
+		System.out.println(listArray.indexOf("Pune"));
 		
-        //Print given element at given index
-        System.out.println(list1.get(1));
-        
-        List list2 = new ArrayList();
-        list2.add("Pune");
-        list2.add("BE");
-        
-        // Add two list
-        list1.addAll(list2);
-        System.out.println(list1);
-        
-        //Check list is empty
-        System.out.println(list1.isEmpty());
-        
-        // Check the element is present in list or not 
-        System.out.println(list1.contains("Rushikesh"));
-        
-        //get the index of element
-        System.out.println(list1.indexOf(7887));
-        
-        //Size of the list
-        System.out.println(list1.size());
-        
-        // remove single element in list
-        System.out.println(list1.remove(1));
-        System.out.println(list1);
-        
-        //remove second list from first list
-        list1.removeAll(list2);
-        System.out.println(list1);
-        
-        // Delete all the elements from the list
-        list1.clear();
-        System.out.println(list1);
+		//3 add by index 
+		listArray.add(2, "Dharashiv");
+		System.out.println(listArray);
+		
+		// 4 exist
+		System.out.println(listArray.contains("Pune"));
+		
+		//5 remove
+		System.out.println(listArray.remove(2));
+		System.out.println(listArray);
+		
 	}
 }
