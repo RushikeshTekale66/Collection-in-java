ddf147cd4323517f1cf061c7d14c155eaec018ce
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-08-22 22:41:30 +0530

    Set - LinkedHashSet

--- Main.java
+++ Main.java
@@ -1,37 +1,37 @@
 import java.util.Set;
-import java.util.HashSet;
+import java.util.LinkedHashSet;
 public class Main
 {
 	public static void main(String[] args) {
-		Set hs = new HashSet();
+		Set lhs = new LinkedHashSet();
 		
-       		 //Add element to HashSet		
-		hs.add(100);
-		hs.add("Rushikesh");
-		hs.add ("Tekale");
-		hs.add("Tekale");
-		System.out.println(hs);
+        		//Add element to LinkedHashSet		
+		lhs.add(100);
+		lhs.add("Rushikesh");
+		lhs.add ("Tekale");
+		lhs.add("Tekale");
+		System.out.println(lhs);
 		
-        		//Add two HashSet
-		Set hs2 = new HashSet();
-		hs2.add("Rohan");
-		hs2.add("Kolegaon");
+       		 //Add two LinkedHashSet
+		Set lhs2 = new LinkedHashSet();
+		lhs2.add("Rohan");
+		lhs2.add("Kolegaon");
 		
-		hs.addAll(hs2);
-		System.out.println(hs);
+		lhs.addAll(lhs2);
+		System.out.println(lhs);
 		
-		//check element present in HashSet
-		System.out.println(hs.contains("Rushikesh"));
+		//check element present in LinkedHashSet
+		System.out.println(lhs.contains("Rushikesh"));
 		
-		//Check HashSet present in HashSet
-		System.out.println(hs.containsAll(hs2));
+		//Check HashSet present in LinkedHashSet
+		System.out.println(lhs.containsAll(lhs2));
 		
-		//remove element in HashSet
-		hs2.remove("Rohan");
-		System.out.println(hs);
+		//remove element in LinkedHashSet
+		lhs2.remove("Rohan");
+		System.out.println(lhs);
 		
-		//size of HashSet
-		System.out.println(hs.size());
+		//size of LinkedHashSet
+		System.out.println(lhs.size());
 		
 	}
 }
