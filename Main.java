fca5ca2dd9238325db56a0ded6300e0c4f5b06f8
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-08-22 22:42:32 +0530

    Set - TreeSet

--- Main.java
+++ Main.java
@@ -1,37 +1,36 @@
 import java.util.Set;
-import java.util.LinkedHashSet;
+import java.util.TreeSet;
 public class Main
 {
 	public static void main(String[] args) {
-		Set lhs = new LinkedHashSet();
+		Set ts = new TreeSet();
 		
-        		//Add element to LinkedHashSet		
-		lhs.add(100);
-		lhs.add("Rushikesh");
-		lhs.add ("Tekale");
-		lhs.add("Tekale");
-		System.out.println(lhs);
+        //Add element to TreeSet
+		ts.add("Rushikesh");
+		ts.add ("Tekale");
+		ts.add("Tekale");
+		System.out.println(ts);
 		
-       		 //Add two LinkedHashSet
-		Set lhs2 = new LinkedHashSet();
-		lhs2.add("Rohan");
-		lhs2.add("Kolegaon");
+        //Add two TreeSet
+		Set ts2 = new TreeSet();
+		ts2.add("Rohan");
+		ts2.add("Kolegaon");
 		
-		lhs.addAll(lhs2);
-		System.out.println(lhs);
+		ts.addAll(ts2);
+		System.out.println(ts);
 		
-		//check element present in LinkedHashSet
-		System.out.println(lhs.contains("Rushikesh"));
+		//check element present in TreeSet
+		System.out.println(ts.contains("Rushikesh"));
 		
-		//Check HashSet present in LinkedHashSet
-		System.out.println(lhs.containsAll(lhs2));
+		//Check TreeSet present in TreeSet
+		System.out.println(ts.containsAll(ts2));
 		
-		//remove element in LinkedHashSet
-		lhs2.remove("Rohan");
-		System.out.println(lhs);
+		//remove element in TreeSet
+		ts2.remove("Rohan");
+		System.out.println(ts);
 		
-		//size of LinkedHashSet
-		System.out.println(lhs.size());
+		//size of TreeSet
+		System.out.println(ts.size());
 		
 	}
 }
