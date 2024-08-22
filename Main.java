2a55c0b91d42ca83b2bd2c3a07727b89c54ceb81
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-08-22 22:46:16 +0530

    Iterator - Interface

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.Iterator;
 public class Main
 {
 	public static void main(String[] args) {
@@ -8,29 +9,13 @@ public class Main
         //Add element to TreeSet
 		ts.add("Rushikesh");
 		ts.add ("Tekale");
+		ts.add("Ramesh");
 		ts.add("Tekale");
-		System.out.println(ts);
-		
-        //Add two TreeSet
-		Set ts2 = new TreeSet();
-		ts2.add("Rohan");
-		ts2.add("Kolegaon");
-		
-		ts.addAll(ts2);
-		System.out.println(ts);
-		
-		//check element present in TreeSet
-		System.out.println(ts.contains("Rushikesh"));
-		
-		//Check TreeSet present in TreeSet
-		System.out.println(ts.containsAll(ts2));
-		
-		//remove element in TreeSet
-		ts2.remove("Rohan");
-		System.out.println(ts);
-		
-		//size of TreeSet
-		System.out.println(ts.size());
-		
+
+		Iterator it = ts.iterator();
+
+		while(it.hasNext()){
+			System.out.println(it.next());
+		}
 	}
 }
