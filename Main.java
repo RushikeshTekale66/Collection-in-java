45e428d5a3ab0ac1e4e8e91c924bc6962ac5e6f4
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-08-22 20:16:35 +0530

    List : Vector

--- Main.java
+++ Main.java
@@ -1,30 +1,30 @@
 import java.util.List;
-import java.util.LinkedList;
+import java.util.Vector;
 public class Main
 {
 	public static void main(String[] args) {
 	    
 	    //Linked List
-		List listLinked = new LinkedList();
+		List listVector = new Vector();
 		
 		//1 .add();
-		listLinked.add("Rushikesh Tekale");
-		listLinked.add(24);
-		listLinked.add("Pune");
+		listVector.add("Rushikesh Tekale");
+		listVector.add(24);
+		listVector.add("Pune");
 		
 		//2 .indexOf();
-		System.out.println(listLinked.indexOf("Pune"));
+		System.out.println(listVector.indexOf("Pune"));
 		
 		//3 add by index 
-		listLinked.add(2, "Dharashiv");
-		System.out.println(listLinked);
+		listVector.add(2, "Dharashiv");
+		System.out.println(listVector);
 		
 		// 4 exist
-		System.out.println(listLinked.contains("Pune"));
+		System.out.println(listVector.contains("Pune"));
 		
 		//5 remove
-		System.out.println(listLinked.remove(2));
-		System.out.println(listLinked);
+		System.out.println(listVector.remove(2));
+		System.out.println(listVector);
 		
 	}
 }
