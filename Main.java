a106c95769d965e02468408d0d11d342f294ec41
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-08-23 12:10:08 +0530

    Sort the list

--- Main.java
+++ Main.java
@@ -1,27 +1,68 @@
-import java.util.List;
 import java.util.ArrayList;
-import java.util.ListIterator;
-public class Main
-{
-	public static void main(String[] args) {
-		List ts = new ArrayList();
+import java.util.Iterator;
+import java.util.List;
+
+class Main{
+	
+	public static void main(String [] args) {
+		Student st = new Student();
+		st.insert();
+		st.sort();
+		st.display();
+	}
+}
+
+
+ class Student {
+	private String name;
+	private int id;
+	private double marks;
+	
+	List <Student> l1 = new ArrayList<Student>();
+	
+	public Student(String name, int id, double marks) {
+		this.name = name;
+		this.id = id;
+		this.marks = marks;
+	}
+	
+	public Student() {};
+	
+	public void insert() {
+		l1.add(new Student("Rushikesh", 101, 81.70));
+		l1.add(new Student("Tushar", 104, 89.90));
+		l1.add(new Student("Rohan", 103, 78.90));
+		l1.add(new Student("Swara", 102, 98.90));
 		
-        //Add element to TreeSet
-		ts.add("Rushikesh");
-		ts.add ("Tekale");
-		ts.add("Ramesh");
-		ts.add("Tekale");
-
-		ListIterator it = ts.listIterator();
-
-		// Forward Iterator
-		while(it.hasNext()){
-			System.out.println(it.next());
+		Iterator i = l1.iterator();
+		System.out.println("Before sorting........................................");
+		while(i.hasNext()) {
+			System.out.println(i.next());
 		}
-
-		//backword Iterator
-		while (it.hasPrevious()) {
-			System.out.println(it.previous());
+	}
+	
+	@Override
+	public String toString() {
+		return "Student Name " + name + " id " + id + " Marks " + marks;
+	}
+	
+	public void sort() {
+		for(int i=0; i<l1.size(); i++) {
+			for(int j=0; j<l1.size(); j++) {
+				if(l1.get(i).marks > l1.get(j).marks) {
+					Student temp = l1.get(i);
+					l1.set(i, l1.get(j));
+					l1.set(j, temp);
+				}
+			}
+		}
+	}
+	
+	public void display() {
+		System.out.println("After sorting...................................");
+		Iterator i = l1.iterator();
+		while(i.hasNext()) {
+			System.out.println(i.next());
 		}
 	}
 }
