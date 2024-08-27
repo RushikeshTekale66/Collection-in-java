310429facbe63fb0d9a68caac3d50ebc05378e17
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-08-27 11:05:08 +0530

    Map : HashMap

--- Main.java
+++ Main.java
@@ -1,57 +1,46 @@
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-
-class Main{
-    public static void main(String [] args){
-        List<Employee> l1 = new ArrayList<Employee>();
-        
-        l1.add(new Employee("XYZ", 103, 1000));
-        l1.add(new Employee("ABC", 102, 2000));
-        l1.add(new Employee("MNO", 101, 4000));
-        l1.add(new Employee("PQR", 104, 3000));
-        
-        System.out.println("Original list");
-        for(Employee e : l1){
-            System.out.println(e);
-        }
-        
-        System.out.println();
-        System.out.println("Sorted List");
-        
-        Collections.sort(l1, new IdComparator());
-        for(Employee e : l1){
-            System.out.println(e);
-        }
-    }
-}
-
-
-//ADD element to the list using constructor
-class Employee{
-    String name;
-    int id;
-    double salary;
-    
-    public Employee(String name, int id, double salary){
-        this.name = name;
-        this.id = id;
-        this.salary = salary;
-    }
-    
-    @Override
-    public String toString(){
-        return "Name : " + name + ", Id : " + id + ", Salary : " + salary;
-    }   
-}
-
-
-//Comparator Interface
-
-class IdComparator implements Comparator<Employee>{
-    @Override
-    public int compare(Employee o1, Employee o2){
-        return o1.id-o2.id;
-    }
+import java.util.Map;
+import java.util.HashMap;
+public class Main
+{
+	public static void main(String[] args) {
+		
+		Map <Integer, String> hm = new HashMap<Integer, String>();
+		
+		//Add element to HashMap
+		hm.put(1, "Rushi");
+		hm.put(5, "Rohan");
+		hm.put(2, "Ramesh");
+		hm.put(4, "Tushar");
+		hm.put(3, "Adinath");
+		
+		System.out.println(hm);
+		
+		//Access value from HashMap
+		System.out.println("Value : "+ hm.get(3));
+		
+		//Remove item form HashMap
+		System.out.println("Deleted Item : " + hm.remove(3));
+		
+		//Size of HashMap
+		System.out.println("Size is : " + hm.size());
+		
+		//Print the keys
+		System.out.println("Kyes are : " + hm.keySet());
+		
+		//Print values
+		System.out.println("Values are : " + hm.values());
+		
+		//Check contains key 
+		System.out.println("Key present : " + hm.containsKey(1));
+		
+		//Check contains values
+		System.out.println("Value present : " + hm.containsValue("Rushi"));
+		
+		//check is empty
+		System.out.println("Is empty : " + hm.isEmpty());
+		
+		//replace/Update the existing key value pair 
+		hm.replace(1, "Sourabh");
+		System.out.println(hm);
+	}
 }
