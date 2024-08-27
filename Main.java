f60105fd70e2adcd0ed6e17e7f0f494762874279
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-08-27 11:09:45 +0530

    Map : Add map element to the set(Convert to map to set

--- Main.java
+++ Main.java
@@ -1,32 +1,36 @@
 import java.util.Map;
 import java.util.HashMap;
+import java.util.Set;
+import java.util.Iterator;
 
-class Main{
-    public static void main(String [] args){
-        Map<Integer, Student> m = new HashMap<Integer, Student>();
+public class Main
+{
+	public static void main(String[] args) {
+		
+		Map <Integer, String> hm = new HashMap<Integer, String>();
+		
+		//Add element to HashMap
+		hm.put(1, "Rushi");
+		hm.put(5, "Rohan");
+		hm.put(2, "Ramesh");
+		hm.put(4, "Tushar");
+		hm.put(3, "Adinath");
+		
+		System.out.println(hm);
+		
+		//Add Map element to the set 
+		System.out.println(hm.entrySet());
+		Set s = hm.entrySet();
+		
+		//Iterate using Iterator
+        Iterator itr = s.iterator();
         
-        m.put(1, new Student("Rushikesh", 10));
-        m.put(3, new Student("Rohan", 20));
-        m.put(2, new Student("Tushar", 30));
+        while(itr.hasNext()){
+            System.out.println(itr.next());
+        }
         
-        m.forEach((k, v)->{
-            System.out.println(k + ", " +  v);
-        });
-    }
-}
-
-
-// Add object to the Map
-class Student{
-    String name;
-    int id;
-    
-    public Student(String name, int id){
-        this.name = name;
-        this.id = id;
-    }
-    
-    public String toString(){
-        return "Name : " + name + " & Id : " + id;
-    }
+        //Add key to the set 
+        System.out.println(hm.keySet());
+		
+	}
 }
