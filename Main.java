c66f2f28f19f8355a7da9c0dded31821f2708ccf
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-08-28 19:53:52 +0530

    Lambda Function

--- Main.java
+++ Main.java
@@ -2,14 +2,12 @@
 public class Main
 {
 	public static void main(String[] args) {
-	    MathOp m = new MathOp(){
-	        @Override
-	        public int add(int a, int b){
-	            return a+b;
-	        }
-	    };
 	    
+	    MathOp m = (int a, int b)->{return a+b;};
 	    System.out.println("Addition of two number is " + m.add(10, 20));
+	    
+	    MathOp m2 = (a, b)->a+b;
+	    System.out.println("Addition is : " + m2.add(10, 20));
 	}
 }
 
