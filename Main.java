e460f848b45c596b73a9fce378dbb7dad35f2b71
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-08-28 19:53:06 +0530

    AnonyMous Class in java

--- Main.java
+++ Main.java
@@ -1,36 +1,19 @@
-import java.util.Map;
-import java.util.HashMap;
-import java.util.Set;
-import java.util.Iterator;
 
 public class Main
 {
 	public static void main(String[] args) {
-		
-		Map <Integer, String> hm = new HashMap<Integer, String>();
-		
-		//Add element to HashMap
-		hm.put(1, "Rushi");
-		hm.put(5, "Rohan");
-		hm.put(2, "Ramesh");
-		hm.put(4, "Tushar");
-		hm.put(3, "Adinath");
-		
-		System.out.println(hm);
-		
-		//Add Map element to the set 
-		System.out.println(hm.entrySet());
-		Set s = hm.entrySet();
-		
-		//Iterate using Iterator
-        Iterator itr = s.iterator();
-        
-        while(itr.hasNext()){
-            System.out.println(itr.next());
-        }
-        
-        //Add key to the set 
-        System.out.println(hm.keySet());
-		
+	    MathOp m = new MathOp(){
+	        @Override
+	        public int add(int a, int b){
+	            return a+b;
+	        }
+	    };
+	    
+	    System.out.println("Addition of two number is " + m.add(10, 20));
 	}
+}
+
+@FunctionalInterface
+interface MathOp{
+    public abstract int add(int a, int b);
 }
