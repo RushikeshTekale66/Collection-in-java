6a679b5885987f9632db333dec6ade8225e5c5d6
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-09-24 08:56:05 +0530

    List : ArrayList

--- Main.java
+++ Main.java
@@ -1,30 +1,21 @@
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Stream;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+class Main{
+	public static void main(String [] args){
+		List al = new ArrayList();
+		al.add("Rushikesh");
+		al.add("Rushikesh");
+		al.add(10);
+		al.add(10);
+		al.add(null);
+		al.add(null);
 
 
-public class Main
-{
-	public static void main(String [] args) {
-		List <Integer> l1 = Arrays.asList(11, 12, 13, 14, 15, 16, 17, 18, 19, 20);
-
-		Stream<Integer> st = l1.stream();
-
-		//Filter method : Filter record & add the result in result1
-		System.out.println("\nfilter()");
-		List<Integer> result1 = st.filter(i->i%2==0).collect(Collectors.toList());
-		System.out.print(result1);
-
-
-        System.out.println("\n\nmap() ");
-		l1.stream().map(num->num*num).forEach((a)->System.out.print(a + ", "));
-		
-		System.out.println("\n\nsorting()");
-		l1.stream().sorted().forEach((a)->System.out.print(a + ", "));
-		
-		System.out.println("\n\nmin()");
-	    Integer result4 = 	l1.stream().min((a, b)->a.compareTo(b)).get();
-	    System.out.println(result4);
+		Iterator it = al.iterator();
+		while (it.hasNext()) {
+			System.out.println(it.next());
+		}
 	}
 }
