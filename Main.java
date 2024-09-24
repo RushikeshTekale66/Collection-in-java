8e3b0f7f0c42140b8aba6a947d59b61ceea8030b
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-09-24 09:50:42 +0530

    Map - Tree map

--- Main.java
+++ Main.java
@@ -1,14 +1,12 @@
-import java.util.HashMap;
+import java.util.TreeMap;
 import java.util.Map;
 public class Main {
 	public static void main(String [] args){
-		Map m = new HashMap();
+		Map m = new TreeMap();
 		m.put(5, "Rushikesh");
 		m.put(4, "Rushikesh");
 		m.put(4, "Rushikesh");
 		m.put(3, "Tekale");
-		m.put(null, null);
-		m.put(null, null);
 
 		System.out.println(m.values());
 		System.out.println(m.keySet());
