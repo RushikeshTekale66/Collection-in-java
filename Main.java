56f30a85c875c2b3031cc7c7d4ae54dd0aadfcbc
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-09-24 09:01:38 +0530

    List - Vector

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 import java.util.List;
-import java.util.LinkedList;
+import java.util.Vector;
 import java.util.Iterator;
 
 class Main{
 	public static void main(String [] args){
-		List al = new LinkedList();
+		List al = new Vector();
 		al.add("Rushikesh");
 		al.add("Rushikesh");
 		al.add(10);
@@ -16,6 +16,10 @@ class Main{
 		Iterator it = al.iterator();
 		while (it.hasNext()) {
 			System.out.println(it.next());
+		}
+
+		for(int i=0; i<al.size(); i++){
+			System.out.println(al.get(i));
 		}
 	}
 }
